0b1d505aed5a89522cdd57818809a8280b3e41a5
chakripavankumar <siriyalapavankumar@gmail.com>
2024-05-07 17:02:49 +0530

    week-9.2✅

--- week-9.2/a.ts
+++ week-9.2/a.ts
@@ -1,3 +1,10 @@
- const x:number=2;
- console.log(x);
- 
+ function isLegal( age:number){
+    if(age<18){
+        return console.log("your not a leegal citizen");
+      
+    } else{
+        return console.log(" hey you're a legal citizen");
+    }
+ }
+  console.log(isLegal(78));
+  
